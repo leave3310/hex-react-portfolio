257fdf68d5460aad6205fce3b3cc08524e9ee456
leave3310 <sampisam3@gmail.com>
2025-05-03 15:55:11 +0800

    Updates

--- layouts/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-white py-[24px] font-medium text-[16px] text-center">
-            © 2025 Alyse Wang. All rights reserved.
-        </footer>
-    );
-}
--- pages/index/index.tsx
+++
@@ -1,9 +0,0 @@
-import Default from "@/layouts/Default";
-
-export default function Index() {
-    return (
-        <Default >
-            <h1>Index</h1>
-        </Default>
-    );
-}
