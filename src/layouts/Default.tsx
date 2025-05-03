1a82c3eed85ccb1328c72866b2bee041bacf9bfc
leave3310 <sampisam3@gmail.com>
2025-05-03 15:00:28 +0800

    feat: 新增 Header、Footer、DefaultLayout

---
+++ src/layouts/Default.tsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default function Default({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex flex-col min-h-screen justify-between bg-soft-mist">
+            <Header />
+            <div className="grow">
+                {children}
+            </div>
+            <Footer />
+        </div>
+    );
+}
