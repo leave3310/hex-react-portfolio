cbe1ba5256d994762b1ceb43cfbfe89a7b2f68a2
leave3310 <sampisam3@gmail.com>
2025-05-09 22:04:35 +0800

    feat: 新增 vite-plugin-svgr 套件

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from "vite-plugin-svgr";
 import { fileURLToPath } from 'url';
 import path from 'path';
 
@@ -12,6 +13,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    svgr()
   ],
   resolve: {
     alias: {
