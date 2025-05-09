4e4b82572d0024d75ececc7ae363e486cd6831b7
leave3310 <sampisam3@gmail.com>
2025-05-09 22:29:56 +0800

    init: 調整 eslint 相關檔案

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,11 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-    typescript: true,
-    react: true,
-    vue: false,
-    stylistic: {
-        indent: 2, // 4, or 'tab'
-        quotes: 'single', // or 'double'
-    },
+  typescript: true,
+  react: true,
+  vue: false,
+  stylistic: {
+    indent: 'tab', // 4, or 'tab'
+    quotes: 'single', // or 'double'
+  },
 })
